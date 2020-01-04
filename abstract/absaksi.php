005faa583573e646b8a2f3bf08191828199b984a
agisabhi28 <agisabhi28@email.unikom.ac.id>
2020-01-04 08:57:33 +0700

    edit abs aksi

--- abstract/absaksi.php
+++ abstract/absaksi.php
@@ -1,16 +1,21 @@
 <?php
 include '../koneksi.php';
-$username   = $_POST['username'];
-$password	= $_POST['password'];
-$password1  = $_POST['password1'];
-$akses	    = $_POST['akses'];
+$title   					= $_POST['title'];
+$authors					= $_POST['authors'];
+$institution  		= $_POST['institution'];
+$isi_abstract	    = $_POST['isi_abstract'];
+$keyword 					= $_POST['keyword'];
+$topic 						= $_POST['topic'];
+$presenter 				= $_POST['presenter'];
+$status_presenter = $_POST['status_presenter'];
+$tipe						  = $_POST['tipe'];
 
-if ($password==$password1) {
-	$query = mysqli_query($koneksi, "insert into akun values('', '$username', '$password', '$akses' )")or die(mysqli_error());
+if (isset($_POST['submit'])) {
+	$query = mysqli_query($koneksi, "insert into akun values('', '$title', '$authors', '$institution', '$isi_abstract', '$keyword', '$topic', '$presenter', '$status_presenter', '$tipe' )")or die(mysqli_error());
 
 if ($query) {
-	echo "<script>alert('Berhasil Daftar'); </script>";
-	header('location:../index.php');
+	echo "<script>alert('Berhasil Input Abstract'); </script>";
+	header('location:index.php');
 
 }else{
 	echo "<center> Gagal Daftar </center>";
